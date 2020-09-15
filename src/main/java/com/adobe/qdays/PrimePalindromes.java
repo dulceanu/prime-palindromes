6899407926c895c4734132a03ab47fe4a7d7eada
Andrei Dulceanu <dulceanu@adobe.com>
2020-09-15 13:40:36 +0200

    Initial Commit - full solution and working tests

---
+++ src/main/java/com/adobe/qdays/PrimePalindromes.java
@@ -0,0 +1,85 @@
+package com.adobe.qdays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Write a program that finds all prime palindromes 
+ * in the range of two supplied numbers a and b (2 <= a < b <= 500,000,000). 
+ *
+ * Palindrome examples: 12321, 123321
+ * 
+ * Prime palindromes example: 101
+ */
+public class PrimePalindromes {
+
+	 List<Integer> generatePalindromes(int a, int b) {
+		List<Integer> palindromes = new ArrayList<>();
+		
+		for (int d = countDigits(a); d <= countDigits(b); d++) {
+			for (int j = 1; j < 10; j++) {
+				generatePalindrome(d, j, palindromes);
+			}
+		}
+
+		return palindromes;
+	}
+
+	private void generatePalindrome(int digits, int n, List<Integer> palindromes) {
+		if (digits == 1) {
+			palindromes.add(n);
+			return;
+		}
+		
+		if (countDigits(n) == digits / 2) {
+			if (digits % 2 == 0) {
+				palindromes.add(Integer.parseInt(n + "" + reverse(n)));
+			} else {
+				for (int i = 0; i < 10; i++) {
+					palindromes.add(Integer.parseInt(n + "" + i + reverse(n)));
+				}
+			}
+		} else {
+			for (int i = 0; i < 10; i++) {
+				generatePalindrome(digits, n * 10 + i, palindromes);
+			}
+		}
+	}
+
+	List<Integer> primePalindromes(int a, int b) {
+		List<Integer> results = new ArrayList<>();
+
+		List<Integer> palindromes = generatePalindromes(a, b);
+		for (Integer pal : palindromes) {
+			if (a<=pal && pal<b && isPrime(pal)) {
+				results.add(pal);
+			}
+		}
+
+		return results;
+	}
+
+	boolean isPrime(int n) {
+		if (n < 2) return false;
+
+		for (long i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	int countDigits(int n) {
+		return Integer.toString(n).length();
+	}
+	
+	boolean isPalindrome(int n) {
+		return String.valueOf(n).equals(reverse(n));
+	}
+	
+	String reverse(int n) {
+		return new StringBuilder("" + n).reverse().toString();
+	}
+}
---
+++ src/test/java/com/adobe/qdays/PrimePalindromesTest.java
@@ -0,0 +1,70 @@
+package com.adobe.qdays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+import com.adobe.qdays.PrimePalindromes;
+
+public class PrimePalindromesTest {
+	private List<Integer> filteringSolution(int a, int b) {
+		List<Integer> primePalindromes = new ArrayList<>();
+		PrimePalindromes pp = new PrimePalindromes();
+
+		for (int i = a; i < b; i++) {
+			if (pp.isPalindrome(i) && pp.isPrime(i)) {
+				primePalindromes.add(i);
+			}
+		}
+
+		return primePalindromes;
+	}
+
+	@Test
+	public void testIsPrime() {
+		PrimePalindromes pp = new PrimePalindromes();
+		assertFalse(pp.isPrime(1));
+		assertTrue(pp.isPrime(2));
+		assertTrue(pp.isPrime(3));
+		assertTrue(pp.isPrime(13));
+		assertFalse(pp.isPrime(49));
+	}
+
+	@Test
+	public void testGeneratePalindromes() {
+		PrimePalindromes mpp = new PrimePalindromes();
+
+		List<Integer> palindromes = mpp.generatePalindromes(1000, 9999);
+		for (Integer pal : palindromes) {
+			assertTrue(mpp.isPalindrome(pal));
+			assertEquals(0, mpp.countDigits(pal) % 2);
+			assertEquals(4, mpp.countDigits(pal));
+		}
+	}
+
+	@Test
+	public void testGeneratingMatchesFiltering() {
+		PrimePalindromes mpp = new PrimePalindromes();
+		int a = 11;
+		int b = 101;
+		assertTrue(filteringSolution(a, b).containsAll(mpp.primePalindromes(a, b)));
+		assertTrue(mpp.primePalindromes(a, b).containsAll(filteringSolution(a, b)));
+
+		Random rand = new Random(42);
+		for (int i = 0; i < 100; i++) {
+			a = rand.nextInt(10000);
+			b = a + rand.nextInt(500_000);
+
+			List<Integer> filteringResults = filteringSolution(a, b);
+			List<Integer> generatingResults = mpp.primePalindromes(a, b);
+			assertTrue(filteringResults.containsAll(generatingResults));
+			assertTrue(generatingResults.containsAll(filteringResults));
+		}
+	}
+}
